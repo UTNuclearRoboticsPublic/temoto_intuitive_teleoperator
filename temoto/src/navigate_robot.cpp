9515967c1542f8559191aa734fad93ee4b3238d4
Karl Kruusamäe <karl.kruusamae@utexas.edu>
2016-09-16 17:17:25 -0500

    untested changes to navigate_robot

--- temoto/src/navigate_robot.cpp
+++ temoto/src/navigate_robot.cpp
@@ -26,10 +26,17 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// ROS includes
-#include "ros/ros.h"
+/** @file navigate_robot.cpp
+ * 
+ *  @brief ROS server that acts as an interface for ROS navigation stack.
+ * 
+ *  @author karl.kruusamae(at)utexas.edu
+ */
 
-// temoto includes
+// TODO TODO TODO TODO TODO TODO TODO
+// Remove servive part, and instead simply access it as a library. Have goal, cancel and get feedback functions.
+// Perhaps some class variables that are updated based on local callbacks.
+
 #include "navigate_robot.h"
 
 /** This method is executed when temoto/navigate_robot_service service is called.
@@ -67,6 +74,36 @@ void NavigateRobotInterface::requestNavigation()
   return;
 }
 
+void NavigateRobotInterface::sendNavigationGoal(geometry_msgs::PoseStamped goal)
+{
+  move_base_msgs::MoveBaseGoal mb_goal;
+
+  mb_goal.target_pose = goal;
+  
+  ROS_INFO("[temoto/navigate_robot] Sending navigation goal");
+  move_base_aclient_.sendGoal(mb_goal);
+
+//   move_base_aclient_.waitForResult();
+
+//   if(move_base_aclient_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+//     ROS_INFO("[temoto/navigate_robot] Navigation goal achieved");
+//   else
+//     ROS_INFO("[temoto/navigate_robot] Navigation goal failed for some reason");
+  return;
+}
+
+void NavigateRobotInterface::abortNavigation()
+{
+  move_base_aclient_.cancelGoal();
+  return;
+}
+
+void NavigateRobotInterface::checkNavigationStatus()
+{
+  move_base_aclient_.getState();
+  return;
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "navigate_robot");
@@ -87,7 +124,7 @@ int main(int argc, char** argv)
   // Set up service for navigate_robot_srv; if there's a service request, call serviceUpdate() function
   ros::ServiceServer service = n.advertiseService("temoto/navigate_robot_srv", &NavigateRobotInterface::serviceUpdate, &navigateIF);
   
-  while (ros::ok())
+  while ( ros::ok() )
   {
     // if new navigation goal has been requested
     if (navigateIF.new_navgoal_requested_)
@@ -98,24 +135,6 @@ int main(int argc, char** argv)
     }
   }
 
-//   move_base_msgs::MoveBaseGoal goal;
-// 
-//   //we'll send a goal to the robot to move 1 meter forward
-//   goal.target_pose.header.frame_id = "base_link";
-//   goal.target_pose.header.stamp = ros::Time::now();
-// 
-//   goal.target_pose.pose.position.x = 1.0;
-//   goal.target_pose.pose.orientation.w = 1.0;
-// 
-//   ROS_INFO("Sending goal");
-//   navigateIF.move_base_aclient_.sendGoal(goal);
-// 
-//   navigateIF.move_base_aclient_.waitForResult();
-// 
-//   if(navigateIF.move_base_aclient_.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-//     ROS_INFO("Hooray, the base moved 1 meter forward");
-//   else
-//     ROS_INFO("The base failed to move forward 1 meter for some reason");
 
   return 0;
 }
