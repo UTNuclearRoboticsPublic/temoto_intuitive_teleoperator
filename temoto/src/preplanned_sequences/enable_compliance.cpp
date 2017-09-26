bd43f5853f565cef7021576790ad4e2188c25bf0
john-a-duncan <30937261+john-a-duncan@users.noreply.github.com>
2017-09-26 12:24:01 -0500

    Update for ur_script_interface.

--- temoto/src/preplanned_sequences/enable_compliance.cpp
+++ temoto/src/preplanned_sequences/enable_compliance.cpp
@@ -13,7 +13,7 @@ int enable_compliance::enable_compliance()
   int type = 1;  // Force frame transform
   std::vector<float> limits {0.8, 0.8, 0.8, 1.571, 1.571, 1.571};  // Displacement limits
 
-  ur_script_compliance right_arm("/right_ur5_controller/right_ur5_URScript");
+  ur_script_interface right_arm("/right_ur5_controller/right_ur5_URScript");
   right_arm.enable_force_mode_( force_frame, selection_vector, target_wrench, type, limits );
 
   return 0;
